dab69a35ac842c6d9e47db47c342fce1a337c14f
Isaac Cummings <Cummingsi1993@gmail.com>
2016-08-11 08:21:47 -0400

    autofocus

--- src/components/ui-input/ui-input.component.ts
+++ src/components/ui-input/ui-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, Provider, forwardRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, Provider, forwardRef, ElementRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, CORE_DIRECTIVES } from '@angular/common';
 
 const noop = () => { };
@@ -14,7 +14,7 @@ const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR = new Provider(
 	template: `
 		<div class="form-group">
 			<label *ngIf="label != null">{{label}}</label>
-			<input [autofocus]="autofocus" [(ngModel)]="model" class="form-control" type="text" placeholder="{{placeholder}}" />
+			<input [(ngModel)]="model" class="form-control" type="text" placeholder="{{placeholder}}" />
 		</div>
 	`,
 	directives: [CORE_DIRECTIVES],
@@ -25,7 +25,7 @@ export class UIInputComponent implements ControlValueAccessor {
 	private _onChangeCallback: (value: any) => void = noop;
 	private _onTouchedCallback: () => void = noop;
 
-	constructor() {
+	constructor(private elementRef: ElementRef) {
 		//this.modelEmitter = new EventEmitter<string>();
 	}
 
@@ -33,6 +33,19 @@ export class UIInputComponent implements ControlValueAccessor {
 	@Input() placeholder: string;
 	@Input() autofocus: boolean;
 	//@Output('model') modelEmitter: EventEmitter<string>;
+
+	ngOnInit() { 
+		if (this.autofocus) { 
+			let node:any = d3.select(this.elementRef.nativeElement)
+				.select('input')
+				.node();
+
+			node.focus();
+				
+			
+		}
+		
+	}
 
 	set model(value:string) {
 		this._model = value;
